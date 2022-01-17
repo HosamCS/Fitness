b2591586bd760f6d9ca470d10c815935c0751f53
HosamCS <hossamhassan77a@gmail.com>
2022-01-17 05:17:10 +0200

    some mapStyle

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 
+
 import React,{useState} from 'react';
 import {SafeAreaView,StyleSheet, Text, TouchableOpacity, View, } from 'react-native';
 import MapView , { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import GetLocation from 'react-native-get-location';
 import haversine from 'haversine-distance';
-
-
-
+import darkmapStyle from './custommap';
+import lightmapStyle from './lightmapstyle';
 
 const FirtLocation = { latitude: 30.367378, longitude: 30.5163834 }
 const EndLocation = { latitude: 30.284204664756928, longitude: 30.41094053998792 } 
 const Distance = haversine(FirtLocation, EndLocation).toFixed(0);
  const Steps = 1/2 * Distance;
 
-
-export default function App (){
-
+export default function App() {
+  const [dark, setDark] = useState('light');
+  
   const [userlocation, setUserLocation] = useState({
     latitude: 37.78825,
     longitude: -122.4324 ,
@@ -37,7 +37,7 @@ export default function App (){
   .then(location => {  
      setUserLocation(location) // set the state to the location
      startbtn=="off"?setStartbtn("on"):setStartbtn("off");
-     
+     console.warn(userlocation.latitude, userlocation.longitude);
   })
   .catch(error => {
       console.warn(error.code, error.message)
@@ -53,46 +53,44 @@ export default function App (){
      
      setUserLocation(location) // set the state to the location
      startbtn =="off"?setStartbtn("on"):setStartbtn("off");
-     console.warn()
+     console.warn(userlocation.latitude, userlocation.longitude);
   })
   .catch(error => {
       console.warn(error.code, error.message)
   })
 } 
   return (
-    
-    <SafeAreaView style={styles.continer}>
-      
+    <View style={styles.continer}>
       <MapView
-       provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-       style={styles.map}
-       showsCompass={true}
-      //  toolbarEnabled={false}
-       scrollEnabled={true}
-       region={{
-         latitude: userlocation.latitude,
-         longitude: userlocation.longitude,
-         latitudeDelta: 0.015,
-         longitudeDelta: 0.0121,
-       }}
-   
-     >
-        <Marker
-            coordinate={{
-              latitude: userlocation.latitude,
-              longitude: userlocation.longitude,
-            }}             
-            title='Hi'
-            description='I am here'
-            opacity={1}
-
-          />
-
-     </MapView>
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        customMapStyle={dark ==='dark' ? darkmapStyle : lightmapStyle }
+        region={{
+          latitude: userlocation.latitude,
+          longitude: userlocation.longitude,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      >
+          <Marker
+              coordinate={{latitude: userlocation.latitude, longitude: userlocation.longitude,}}             
+              title='Hi'
+              description='I am here'
+              opacity={1}
+            />
+      </MapView>
+      <View style={[styles.buttonContainer,{backgroundColor:dark ==='light'?'#bbbb':'#000'}]}>
+       <TouchableOpacity
+          onPress={() => setDark(dark === 'light' ? 'dark' : 'light')}
+         >
+           {dark === 'light' ?<Text style={{color:'#fff',fontSize:23}}>🌞</Text> 
+           :<Text style={{color:'#fff',fontSize:23}}>🌙</Text>} 
+        </TouchableOpacity>
+      </View>
+      
      
-
-  <View style={{flexDirection:'row',justifyContent:'space-around',marginTop:30,height:120}}>
-  <View >
+     <View style={{flexDirection:'row',justifyContent:'space-around',marginTop:30,height:70,}}>
+        <View >
         <Text style={[styles.txt,{color:'#bbb',fontSize:20}]}>01:30:10⌚</Text>
         <Text style={styles.txt}>Timer</Text>
      </View>
@@ -101,7 +99,7 @@ export default function App (){
         <Text style={styles.txt}>Distance</Text>
      </View>
   </View>
-      <View style={{justifyContent:'center',alignItems:'center'}}>
+      <View style={{justifyContent:'center',alignItems:'center',}}>
          <Text style={[styles.txt,{color:'#bbb',fontSize:20}]}>{Steps} 👣</Text>
          <Text style={styles.txt}>Steps</Text>
       </View>
@@ -113,9 +111,9 @@ export default function App (){
         <Text style={styles.txt}>off</Text>
       </TouchableOpacity> }
     </View>
-    </SafeAreaView>
+    </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   continer: {
@@ -130,7 +128,7 @@ const styles = StyleSheet.create({
   map: {
     alignItems: 'center',
     width: "100%",
-    height: '50%',
+    height: '60%',
   
   },
   btn: {
@@ -154,9 +152,22 @@ const styles = StyleSheet.create({
   },
   activeOff:{
     borderColor:'red',borderWidth:2
+  },
+  buttonContainer:{
+    
+    height: 40,
+    borderRadius: 20,
+    width: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    marginTop: 20,
+    alignSelf: "flex-end",
+    right: 20,
   }
 
   
 });
 
  
+
---
+++ lightmapstyle.js
@@ -0,0 +1,72 @@
+const lightmapStyle = [
+  
+    {
+       "featureType": "landscape",
+       "elementType": "labels",
+       "stylers": [
+           {
+               "visibility": "off"
+           }
+       ]
+   },
+   {
+       "featureType": "poi",
+       "elementType": "labels",
+       "stylers": [
+           {
+               "visibility": "off"
+           }
+       ]
+   },
+   {
+       "featureType": "road",
+       "elementType": "geometry",
+       "stylers": [
+           {
+               "lightness": 57
+           }
+       ]
+   },
+   {
+       "featureType": "road",
+       "elementType": "labels.text.fill",
+       "stylers": [
+           {
+               "visibility": "on"
+           },
+           {
+               "lightness": 22
+           }
+       ]
+   },
+   {
+       "featureType": "road",
+       "elementType": "labels.icon",
+       "stylers": [
+           {
+               "visibility": "off"
+           }
+       ]
+   },
+   {
+       "featureType": "transit",
+       "elementType": "labels",
+       "stylers": [
+           {
+               "visibility": "off"
+           }
+       ]
+   },
+   {
+       "featureType": "water",
+       "elementType": "labels",
+       "stylers": [
+           {
+               "visibility": "off"
+           }
+       ]
+   }
+ 
+]
+
+export default lightmapStyle;
